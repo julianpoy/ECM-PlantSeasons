baa346a4d75fe1f4dce8b8949f328d6e85dff084
Julian Poyourow <julianpoyo@gmail.com>
2015-08-18 22:45:58 -0700

    Ripped vegetables into file with custom algorithm

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,6 +2,89 @@ angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope) {
 
+    $scope.plants = getCookie('plantData');
+
+    $scope.doThang = function(){
+
+        var file = document.getElementById('file').files[0];
+
+        var reader = new FileReader();
+        reader.onload = function(progressEvent){
+
+
+            var plants = [];
+            if(getCookie('plantData')){
+                console.log("Reading job");
+                console.log(JSON.parse(getCookie('plantData')));
+                plants = JSON.parse(getCookie('plantData'));
+            }
+            var month = document.getElementById("month").value;
+            // By lines
+            var lines = this.result.split('\n');
+            for(var line = 0; line < lines.length; line++){
+                lines[line]
+                plantIndex = -1;
+                for(var i = 0; i < plants.length; i++){
+                  if(plants[i].name == lines[line]){
+                      plantIndex = i;
+                      break;
+                  }
+                }
+                if(plantIndex == -1){
+                    var temp = { name: lines[line], primeMonths: [] }
+                    temp.primeMonths.push(month);
+                    plants.push(temp);
+                } else {
+                    var containsMonth = false;
+                    for(var j = 0; j < plants[plantIndex].primeMonths.length; j++){
+                        if(month == plants[plantIndex].primeMonths[j]){
+                            containsMonth = true;
+                        }
+                    }
+                    if(!containsMonth){
+                        plants[plantIndex].primeMonths.push(month);
+                    }
+                }
+
+            }
+            console.log("Saving job");
+            console.log(plants);
+            if(month == "delete"){
+                plants = [];
+                console.log("deleting");
+            }
+            createCookie('plantData', JSON.stringify(plants));
+        };
+        reader.readAsText(file);
+    };
+
+    var createCookie = function(name, value, days) {
+        var expires;
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        }
+        else {
+            expires = "";
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function getCookie(c_name) {
+        if (document.cookie.length > 0) {
+            c_start = document.cookie.indexOf(c_name + "=");
+            if (c_start != -1) {
+                c_start = c_start + c_name.length + 1;
+                c_end = document.cookie.indexOf(";", c_start);
+                if (c_end == -1) {
+                    c_end = document.cookie.length;
+                }
+                return unescape(document.cookie.substring(c_start, c_end));
+            }
+        }
+        return "";
+    }
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
