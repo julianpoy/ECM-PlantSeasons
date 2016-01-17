265a19b7fda8f6c05f1524c1d513b0c02991d289
Julian Poyourow <julianpoyo@gmail.com>
2016-01-16 17:25:35 -0800

    Cleaned up and removed unncessary empty controllers

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -14,9 +14,6 @@ angular.module('starter.controllers', [])
     };
 })
 
-.controller('HomeCtrl', function($scope) {
-})
-
 .controller('MonthCtrl', function($scope, $stateParams, Months, Plants) {
     var monthId = $stateParams.id
     $scope.monthName = Months.all()[monthId];
@@ -43,37 +40,6 @@ angular.module('starter.controllers', [])
 
 .controller('PlantsCtrl', function($scope, Plants) {
     $scope.plants = Plants.all();
-})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
-
-.controller('AboutCtrl', function($scope) {
-})
-
-.controller('ECMCtrl', function($scope) {
 })
 
 .controller('ToolsCtrl', function($scope) {
